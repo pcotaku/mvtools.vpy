f5295b720511a45cea71378fbc32395a469aa937
aniotaweeb <143455624+aniotaweeb@users.noreply.github.com>
2024-02-21 22:19:09 -0500

    Update mvtools.lua

--- mvtools.lua
+++ mvtools.lua
@@ -1,7 +1,7 @@
 #--A fork of A fork of maoiscat's "mvtools_3.py" script featured in the awesome-mpv script compilation and nand's original "mvtools.vpy" script.
 #--Remember to change the file extension in your mpv.conf vapoursynth directory. Example: vf=vapoursynth="C:\Users\<USERNAME>\AppData\Roaming\mpv\scripts\mvtools.lua"
 #--This script retains the integrity of the frame rate conversion option found in the original script while using maoiscat's logical operations.
-#--I've commented out code with a hashtag and two hyphens so my comments don't pass through the compiler.
+#--I've commented out the following code with a hashtag and two hyphens so my comments don't pass through the compiler.
 import vapoursynth as vs
 core = vs.core
 clip = video_in.resize.Bicubic(format=vs.YUV420P8)
