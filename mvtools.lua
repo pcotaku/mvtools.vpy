4a070e865863ec370ea529d43aa9c670b35496f5
aniotaweeb <143455624+aniotaweeb@users.noreply.github.com>
2024-02-21 22:36:29 -0500

    Update mvtools.lua

--- mvtools.lua
+++ mvtools.lua
@@ -4,18 +4,15 @@ clip = video_in.resize.Bicubic(format=vs.YUV420P8)
 vden = 1000
 vfps = container_fps*vden
 dfps = display_fps*vden
-
 if not (clip.width > 3000 or clip.height > 2000 or container_fps > 240):
     vfps_num = int(container_fps * 1e8)
     vfps_den = int(1e8)
     dfps_num = int(dfps * 1e4)
     dfps_den = int(1e4)
-
     clip = core.std.AssumeFPS(clip, fpsnum=vfps, fpsden=vden)
     print("Reflowing from ",vfps/vden," fps to ",dfps_num/dfps_den," fps.")
     super = core.mv.Super(clip, pel=2, sharp=0, rfilter=2)
     mvfw = core.mv.Analyse(super, blksize=32, isb=False, search=3, dct=5)
     mvbw = core.mv.Analyse(super, blksize=32, isb=True,  search=3, dct=5)
     clip = core.mv.FlowFPS(clip, super, mvbw, mvfw, num=dfps, den=vden, mask=1)
-
 clip.set_output()
